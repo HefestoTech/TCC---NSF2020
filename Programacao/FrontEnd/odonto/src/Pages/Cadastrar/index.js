775587e7b56713468e023c826eaee1cb47fcb77b
MateusWill <matilegal@gmail.com>
2020-10-23 19:53:05 -0300

    tela cadas

--- Programacao/FrontEnd/odonto/src/Pages/Cadastrar/index.js
+++ Programacao/FrontEnd/odonto/src/Pages/Cadastrar/index.js
@@ -5,8 +5,23 @@ import './cadastrar.css';
  {
      return(
 
-        <div>
-             Cadastrar na conta
+        <div className="ContCad">
+            <div className="bodyCad">
+                <div className="Tt1cad"><h1>Crie seu perfil</h1></div>
+                <div className="FormPers">
+                    <div className="Tt2Cad">
+                        <h3>Insira seus Dados Pessoais</h3>
+                        <h5 className="obr">Os campos com(*)são obrigatórios</h5>
+                    </div>
+
+                    <div className="form1"></div>
+
+                </div>
+
+
+                <div className="FormRegist"></div>
+            </div>
+             
         </div>
          
 
