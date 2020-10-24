96334dfb0213bfb113ea280114c1d162bb391d5a
MateusWill <matilegal@gmail.com>
2020-10-23 21:48:09 -0300

    tela de cadastr. metade

--- Programacao/FrontEnd/odonto/src/Pages/Cadastrar/index.js
+++ Programacao/FrontEnd/odonto/src/Pages/Cadastrar/index.js
@@ -13,9 +13,21 @@ import './cadastrar.css';
                         <h3>Insira seus Dados Pessoais</h3>
                         <h5 className="obr">Os campos com(*)são obrigatórios</h5>
                     </div>
-                    <div className=""></div>
+telad
+                    <div className="line1">
+                        <div className="formName">
+                            <h5>Nome Completo*</h5>
+                            <input type="text" class="form-control" />
+                        </div>
 
-                    <div className="form1"></div>
+                        <div className="formSexo">
+                            <h5>Selecione seu Sexo*</h5>
+                            <input type="radio" name="sexo"/>
+                            <input type="radio" name="sexo" />
+                        </div>
+                    </div>
+
+                    <div className="form2"></div>
 
                 </div>
 
