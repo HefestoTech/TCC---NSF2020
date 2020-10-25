72a80a449b9f678f44a3caaf58a4a46b288a2c77
Matheus Pinheiro <mplbarbosa@gmail.com>
2020-10-25 16:27:52 -0300

    'oi'

---
+++ Programacao/FrontEnd/odonto/src/Pages/agendarconsultacliente/index.js
@@ -0,0 +1,33 @@
+
+import react from 'react';
+import {Link} from 'react-router-dom';
+import Dentista from '../../Assets/dentista.jpg';
+import Odonto from '../../Assets/odonto.png'
+import './agenda.css'
+import Menu from '../../Components/Menu'
+import Rodape from '../../Components/Footer'
+
+export default function agendarconsultacliente (){
+    return (
+
+        <div>
+           <Menu/>
+
+            <div>
+
+
+
+            </div>
+
+
+        </div>
+       
+      
+           
+           
+
+       
+
+    )
+
+}   
--- Programacao/FrontEnd/odonto/src/Routes.js
+++ Programacao/FrontEnd/odonto/src/Routes.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 import App from './Pages/Home'
-import Login from './Pages/Login/index.js'
+import Login from './Pages/Login'
 import Remarcar from './Pages/Remarcar'
 import Cadastrar from './Pages/Cadastrar'
+import agendarconsultacliente from './Pages/agendarconsultacliente'
+
 
 export default function Routes() {
     return(
@@ -14,6 +16,7 @@ export default function Routes() {
               <Route path="/login" exact component={Login} />
               <Route path="/remarcar" exact component={Remarcar} />
               <Route path="/cadastrar" exact component={Cadastrar} />
+              <Route path="/agendarconsultacliente" exact component={agendarconsultacliente}/>
           </Switch>
         </BrowserRouter>
     )
