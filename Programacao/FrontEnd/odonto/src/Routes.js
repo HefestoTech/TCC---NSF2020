8dd6715e554509cced3d384c678a64f2fa8f26d2
MateusWill <matilegal@gmail.com>
2020-10-25 13:40:46 -0300

    atual

--- Programacao/FrontEnd/odonto/src/Routes.js
+++ Programacao/FrontEnd/odonto/src/Routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 import App from './Pages/Home'
-import Login from './Pages/Login/index.js'
+import Login from './Pages/Login'
 import Remarcar from './Pages/Remarcar'
 import Cadastrar from './Pages/Cadastrar'
 
