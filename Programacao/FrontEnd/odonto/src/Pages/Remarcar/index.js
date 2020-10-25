ee12355671d64df7ae87542614cbacd285cd9ddc
Felipe Conceição <felipecsilva207@gmail.com>
2020-10-25 13:48:50 -0300

    concertei alguns erros no back

--- Programacao/FrontEnd/odonto/src/Pages/Remarcar/index.js
+++ Programacao/FrontEnd/odonto/src/Pages/Remarcar/index.js
@@ -64,7 +64,7 @@ export default function Remarcar(props){
                     </div>
                 </div>
             </div>
-           
+            <ToastContainer/>
             <Rodape/>
         
         </div>
