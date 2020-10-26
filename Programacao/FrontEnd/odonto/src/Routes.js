10006b21cdf715988cf8820e848b8a591cf692cf
infoa34 <infoa34@edu.frei>
2020-10-26 11:00:06 -0300

    arruemei rota

--- Programacao/FrontEnd/odonto/src/Routes.js
+++ Programacao/FrontEnd/odonto/src/Routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
-import Home from './Pages/Home'
-import Login from './Pages/Login/index.JS'
+import Home from './Pages/Home';
+import Login from './Pages/Login/index.js'
 import Remarcar from './Pages/Remarcar'
 import Cadastrar from './Pages/Cadastrar'
 import agendarconsultacliente from './Pages/agendarconsultacliente'
