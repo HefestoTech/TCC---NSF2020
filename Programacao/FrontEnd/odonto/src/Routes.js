7153cbceb235e99057d932df29b984b75dab3d10
infoa34 <infoa34@edu.frei>
2020-10-26 08:37:34 -0300

    aa

--- Programacao/FrontEnd/odonto/src/Routes.js
+++ Programacao/FrontEnd/odonto/src/Routes.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 import Home from './Pages/Home'
-import Login from './Pages/Login'
+import Login from './Pages/Login/index.js'
 import Remarcar from './Pages/Remarcar'
 import Cadastrar from './Pages/Cadastrar'
 import agendarconsultacliente from './Pages/agendarconsultacliente'
+import Menu from "./Pages/OneMenu"
 
 
 export default function Routes() {
@@ -17,6 +18,7 @@ export default function Routes() {
               <Route path="/remarcar" exact component={Remarcar} />
               <Route path="/cadastrar" exact component={Cadastrar} />
               <Route path="/agendarconsultacliente" exact component={agendarconsultacliente}/>
+              <Route path="/menu/:id" exact component={Menu}/>
           </Switch>
         </BrowserRouter>
     )
