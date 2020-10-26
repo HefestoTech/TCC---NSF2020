11c43eb78fc2a2abe3d96ed264f9c3da49e81241
ArthurzinLindo <juvenalfp14@gmail.com>
2020-10-25 23:12:20 -0300

    fiz tudo nessa merda

---
+++ Programacao/FrontEnd/odonto/src/Pages/OneMenu/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Menu from '../../Components/Menu';
+import './menu.css';
+import Fotozinha from '../../Assets/deu tudo errado.png';
+
+ export default function onepagesmenu ()
+ {
+     return(
+
+        <div> 
+            <Menu/>
+
+                 <div className= "fundo">
+
+                    <div className = "titulotwo" ><h2>A SAÚDE COMEÇA PELA BOCA!</h2>
+                    
+                    <div >
+                        <img className= "foto" src = {Fotozinha}></img>
+                    </div>
+                    
+                    <div className = "botaolindo">
+                    <button type="button" class="btn btn-outline-danger">Meus Agendamentos</button>
+                    <button type="button" class="btn btn-outline-danger">Novo Agendamento</button>
+                    </div>
+                    </div>
+
+                </div>
+            
+            
+        </div>
+
+        
+     )
+ }
+
