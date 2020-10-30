e2011f12f3dfab7e39d1017de6a9ff79a9a466fe
Felipe Conceição <felipecsilva207@gmail.com>
2020-10-30 16:07:29 -0300

    aa

--- Programacao/FrontEnd/odonto/src/Components/Loading/index.js
+++ Programacao/FrontEnd/odonto/src/Components/Loading/index.js
@@ -5,7 +5,7 @@ import './index.css'
 export default function Loading () {
     return(
          <div className="mostrarSpin">
-                              <ReactLoading type={"spin"} color={"rgb(46, 143, 223)"} height={70} width={95} />
-                          </div>    
+                <ReactLoading type={"spin"} color={"rgb(46, 143, 223)"} height={70} width={95} />
+         </div>    
     )
 }
