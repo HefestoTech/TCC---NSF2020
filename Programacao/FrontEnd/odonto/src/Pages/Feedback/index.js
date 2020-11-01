47121334bd853e1c6e65ad0a9538fe5f9f5da9ea
Felipe Conceição <felipecsilva207@gmail.com>
2020-11-01 10:12:52 -0300

    estrela muda de cor

--- Programacao/FrontEnd/odonto/src/Pages/Feedback/index.js
+++ Programacao/FrontEnd/odonto/src/Pages/Feedback/index.js
@@ -4,7 +4,36 @@ import Menu from '../../Components/Menu';
 import Rodape from '../../Components/Footer'
 import './feed.css';
 import { Router } from 'react-router-dom';
-export default function Feedback(){
+export default function Feedback() {
+
+    const mudarCorAoClicar = (posicao) => {
+       const list = document.getElementsByTagName("ul")[0];
+       
+        
+       for(let i = 4; i >= posicao; i--)
+        {
+          console.log(posicao);
+          console.log(i);
+          const x = list.getElementsByTagName("li")[i];
+          const f = x.getElementsByTagName("i")[0];
+          f.style.color="black"
+        }
+        
+        for(let a = 0; a < posicao; a++)
+        {
+            console.log(posicao);
+            console.log(a);
+
+            const x = list.getElementsByTagName("li")[a];
+            const f = x.getElementsByTagName("i")[0];
+            f.style.color="rgb(199, 169, 0)"
+        }     
+
+
+    }
+
+
+    
     return(
         <div>
         <Menu></Menu>
@@ -15,14 +44,15 @@ export default function Feedback(){
                     <div className="divStarFeedback">
                        <h2>Avaliar última consulta </h2>
                             <div className="star">
-                            <i class="far fa-star"></i>
-                            <i class="far fa-star"></i>
-                            <i class="far fa-star"></i>
-                            <i class="far fa-star"></i>
-                            <i class="far fa-star"></i>                      
+                                <ul>
+                                <li type="none"><i id="star1"  onClick={() => mudarCorAoClicar(0)} class="far fa-star"></i></li>
+                                <li type="none"><i id="star2"  onClick={() => mudarCorAoClicar(1)} class="far fa-star"></i></li>
+                                <li type="none"><i id="star3"  onClick={() => mudarCorAoClicar(2)} class="far fa-star"></i></li>
+                                <li type="none"><i id="star4"  onClick={() => mudarCorAoClicar(3)} class="far fa-star"></i></li>
+                                <li type="none"><i id="star5"  onClick={() => mudarCorAoClicar(4)} class="far fa-star"></i></li>
+                                </ul>
+                            </div>
                     </div>
-                        
-                        </div>
                
 
                         <div class="coment">                          
