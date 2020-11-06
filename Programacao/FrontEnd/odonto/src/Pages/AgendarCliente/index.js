9dfee14252ed9573e70a8f519613d678981fa43e
MateusWill <matilegal@gmail.com>
2020-11-05 23:07:30 -0300

    tela de agendar

--- Programacao/FrontEnd/odonto/src/Pages/AgendarCliente/index.js
+++ Programacao/FrontEnd/odonto/src/Pages/AgendarCliente/index.js
@@ -9,9 +9,9 @@ import { Link } from 'react-router-dom';
 
 export default function AgendarConsultaCliente () {
     return(
-        <div className="ContAgendar">
+        <div className="ContAgendar backg">
             <Link to="/" ><h1 className="TtLogo">ODONTO</h1></Link>
-            <div className="BodyAgend">
+            <div className="BodyAgend shadow-lg p-3 mb-5 bg-white rounded">
                 <div className="TtAgend">
                     <h1>Agende sua Consulta<img src={Dente} /></h1>
                     <h4>Os campos com(*) são obrigratórios</h4>
@@ -34,12 +34,16 @@ export default function AgendarConsultaCliente () {
                         <div className="lineAgend2">
                             <div className="formServ">
                                 <h5>Selecione um serviço:</h5>
-                                <input type="text" className="form-control" />
+                                <select className="form-control" >
+                                    <option>Limpeza</option>
+                                </select>
                             </div>
 
                             <div className="formProf">
                                 <h5>Escolha um profissional:</h5>
-                                <input type="text" className="form-control" />
+                                <select className="form-control" >
+                                    <option>Mauricio</option>
+                                </select>
                             </div>
                         </div>
                 </div>
@@ -49,10 +53,10 @@ export default function AgendarConsultaCliente () {
 
                     <div className="linePag1">
                         <div className="formPagm">
-                            <h5>Selecione a forma de Pagamento</h5>
-                            <div className="radPag custom-control custom-radio custom-control-inline">
+                        <h5>Selecione a forma de Pagamento:</h5>
+                            <div className="radPag  custom-radio custom-control-inline">
                                 
-                                <input type="radio" name="pagm"
+                                <input type="radio" name="pagm" className="din"
                                 /> <h5>Dinheiro</h5>
 
 
@@ -60,6 +64,34 @@ export default function AgendarConsultaCliente () {
                                 /> <h5>Cartão</h5>
                                 
                             </div>
+                        </div>
+                    </div>
+
+                    <div className="linePag2">
+                        <div className="formParc">
+                            <h5>Quantidade de parcelas:</h5>
+                            <input type="number" className="qtdParc form-control" min="1" max="8" />
+                        </div>
+
+                        <div className="formSub">
+                            <h5>SubTotal:</h5>
+                            <input type="text" readOnly className="Subt form-control" placeholder="R$ 0,00" />
+                        </div>
+                    </div>
+
+                    <div className="linePagm3">
+                        <div className="formDesc">
+                            <h5>Desconto:</h5>
+                            <input type="text" className="Desc form-control" placeholder="R$ 0,00" />
+                        </div>
+
+                        <div className="formTot">
+                            <h5>Total:</h5>
+                            <input type="text" readOnly className="Total form-control" placeholder="R$ 0,00"/>
+                        </div>
+
+                        <div className="ButtPag">
+                            <button className="btnCad btn btn-primary"><h4>Agendar</h4></button>
                         </div>
                     </div>
 
