6cfc29c8cb085fd7209ceee447efdb62bb9bd477
ArthurzinLindo <juvenalfp14@gmail.com>
2020-11-08 17:11:19 -0300

    nada

--- Programacao/FrontEnd/odonto/src/Pages/SobreDev/index.js
+++ Programacao/FrontEnd/odonto/src/Pages/SobreDev/index.js
@@ -4,7 +4,16 @@ import './dev.css'
 export default function SobreDev() {
     return(
         <div class="fundodev">
+            <div class="devOne"> 
+            <i class="fab fa-hire-a-helper"></i><h1 class="hOne">efesto Tech Developers</h1>
+            </div>
             
+            <div class="devTheer">
+                
+                <h2></h2>
+                <p> </p>
+
+            </div>
         </div>
     )
 }
