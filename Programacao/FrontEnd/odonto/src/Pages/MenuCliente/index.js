e3a1878105d5f57b5ae1b025acda939e101d015b
MateusWill <matilegal@gmail.com>
2020-11-11 03:45:17 -0300

    nova telinha de menuCli

--- Programacao/FrontEnd/odonto/src/Pages/MenuCliente/index.js
+++ Programacao/FrontEnd/odonto/src/Pages/MenuCliente/index.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import TelaMenu from "../../Components/TelaMenuOne";
+import User from "../../Assets/Fotos/user.svg"
+import Logout from "../../Assets/Fotos/logout.svg"
+import Brush from "../../Assets/Fotos/brushing-teeth.svg"
+import DentistW from "../../Assets/Fotos/dentist2.svg"
+import './menucli.css'
 
  export default function MenuCliente(props)
  { 
@@ -14,7 +19,7 @@ import TelaMenu from "../../Components/TelaMenuOne";
                    state:responseCompleto
             });
            
-        }
+          }
 
         const irParaTelaDeAgendarNovaConsulta = () => {
               history.push({
@@ -27,16 +32,65 @@ import TelaMenu from "../../Components/TelaMenuOne";
 
 
        return (
-         <TelaMenu>
-           <button onClick={irParaTelaDeVerAgendamentos} type="button" className="bt1 btn btn-danger ">
-             Meus Agendamentos
-           </button>
+         <div className="ContCli">
 
-           <button onClick={irParaTelaDeAgendarNovaConsulta} type="button" className="bt1 btn btn-danger ">
-             Agendar Consulta
-           </button>
+          <div className="HeadMC">
+            <div className="LogoMC"><h1>Odonto</h1></div>
 
-         </TelaMenu>
+            <div className="User"> <img src={User} /> <h5>Seja bem-vindo</h5> </div>
+
+            <Link to="/"><div className="Logout"><img src={Logout} /></div></Link>
+
+
+          </div>
+
+          <div className="BodyMC">
+                <div className="Tt1MC">
+                  <h2>A SAÚDE COMEÇA PELA BOCA</h2>
+                </div>
+
+                <div className="lay1MC">
+                  <div className="leftTriangle"> </div>
+                    <div className="imgBru"><img src={Brush}/></div> 
+                    <div className="Tt2MC"><h2>A Odontologia é a área da saúde que preserva e restaura o movimento mais Lindo do ser humano: O SORRISO.</h2> </div>
+                </div>
+                
+                <div className="lay2MC">
+                  <div className="Box1MC">
+                    <div className="MyAgend">
+                      <h4>Clique aqui para ver seus agendamentos de cada consulta!</h4>
+                      <button onClick={irParaTelaDeVerAgendamentos} type="button" className="bt1MC btn btn-danger ">
+                        Meus Agendamentos
+                      </button>
+
+                    </div>
+                  </div>
+                  
+                  <div className="Box2MC">
+                    <div className="imgDent">
+                      <img src={DentistW} />
+                      <div className="Tt3Dent">
+                        <h5>Dentistas posssuem mãos mágicas que devolvem às pessoas razões para brilhar seu sorriso.</h5>
+                      </div>
+                    </div>
+                  </div>
+                  
+                  <div className="Box3MC">
+                    <div className="MyQuery">
+                    <h4>Caso não tenha um agendamento CLIQUE AQUI!!</h4>
+                    <button onClick={irParaTelaDeAgendarNovaConsulta} type="button" className="bt2MC btn btn-danger ">
+                      Agendar Consulta
+                    </button>
+                    </div>
+
+                  </div>
+                </div>
+                
+          </div>
+
+         </div>
+
+        
        );
  }
 
