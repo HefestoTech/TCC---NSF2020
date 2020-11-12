60ba7b6106d118d1c4b6ba98da383b72197db0a3
MateusWill <matilegal@gmail.com>
2020-11-12 01:41:51 -0300

    tela de agend/func quasefin

--- Programacao/FrontEnd/odonto/src/Pages/AgendarFunc/index.js
+++ Programacao/FrontEnd/odonto/src/Pages/AgendarFunc/index.js
@@ -128,115 +128,126 @@ export default function AgendarConsultaCliente (props) {
         <div className="ContAgendarF backgr">
             <ToastContainer/>
             
-            <div className="BodyAgendF shadow-lg p-3 mb-5 bg-white rounded">
+            <div className="BodyF shadow-lg p-3 mb-5 bg-white rounded">
                 <div className="TtAgendF">
                 <Link to={{ pathname: "/menu/funcionario", state: responseCompleto}} ></Link><h1>Agende sua Consulta</h1>
                 </div>
 
-                <div className="BoxAgenFd">
-                  
-                        <div className="lineAgend1F">
-                            <div className="formDateF">
-                                <h5>Selecione uma data:</h5>
-                                <input type="date" className="form-control" 
-                                onChange={(e) => pegarProfissional(e.target.value, hora)}
-                                />
+                <div className="BodyAgendF">
+                    <div className="BoxAgendF">                  
+                            <div className="lineAgend1F">
+                              <div className="Tt2AgendF">
+                                  <h4>Os campos marcados com(*) são obrigatórios.</h4>
+                              </div>
                             </div>
 
-                            <div className="formHourF">
-                                <h5>Selecione uma hora:</h5>
-                                <input type="time" className="form-control" 
-                                onChange={e => pegarProfissional(date, e.target.value)}
-                                />
+                            <div className="lineAgend2F">
+                                <div className="formDateF">
+                                    <h5>Selecione uma data:</h5>
+                                    <input type="date" className="form-control" 
+                                    onChange={(e) => pegarProfissional(e.target.value, hora)}
+                                    />
+                                </div>
+
+                                <div className="formHourF">
+                                    <h5>Selecione uma hora:</h5>
+                                    <input type="time" className="form-control" 
+                                    onChange={e => pegarProfissional(date, e.target.value)}
+                                    />
+                                </div>
+
                             </div>
 
-                        </div>
+                            <div className="lineAgend3F">
+                                <div className="formServF">
+                                    <h5>Selecione um serviço:</h5>
+                                    <select onChange={(e) => pegarValorDaConsulta(Number(e.target.value), formpagm)} className="form-control" >
+                                        <option value="0"></option>
+                                        {servico.map(x => <option value={x.idServico}>{x.nomeServico}</option> )}
+                                    </select>
+                                </div>
 
-                        <div className="lineAgend2F">
-                            <div className="formServF">
-                                <h5>Selecione um serviço:</h5>
-                                <select onChange={(e) => pegarValorDaConsulta(Number(e.target.value), formpagm)} className="form-control" >
-                                    <option value="0"></option>
-                                    {servico.map(x => <option value={x.idServico}>{x.nomeServico}</option> )}
-                                </select>
+                                <div className="formProfF">
+                                    <h5>Escolha um profissional:</h5>
+                                    <select onChange={e => setIdFuncionario(Number(e.target.value))} className="form-control" >
+                                        <option value="0"></option>
+                                        {profissional.map (x => 
+                                        <option value={x.id}>{x.nome}</option>
+                                        )}
+                                    </select>
+                                </div>
                             </div>
 
-                            <div className="formProfF">
-                                <h5>Escolha um profissional:</h5>
-                                <select onChange={e => setIdFuncionario(Number(e.target.value))} className="form-control" >
-                                    <option value="0"></option>
-                                    {profissional.map (x => 
-                                     <option value={x.id}>{x.nome}</option>
-                                    )}
-                                </select>
+                            <div className="lineAgend4F">
+                                <div className="formeE-mailF">
+                                    <h5>Digite o e-mail do cliente:</h5>
+                                    <input onChange={e => setEmailCliente(e.target.value)} class="form-control mr-sm-2 btnnTwo" type="search" placeholder="Fulano@example.com" aria-label="Pesquisar" />
+                                </div>
                             </div>
-                        </div>
-
-                        <div className="lineAgend3F">
-                            <div className="formeE-mailF">
-                                <h5>Digite o e-mail do cliente:</h5>
-                                <input onChange={e => setEmailCliente(e.target.value)} class="form-control mr-sm-2 btnnTwo" type="search" placeholder="Fulano@example.com" aria-label="Pesquisar" />
-                            </div>
-                        </div>
-                </div>
-
-                <div className="BoxPagmtF">
-                    <h2>Pagamento da consulta</h2>
-
-                    <div className="linePag1F">
-                        <div className="formPagmF">
-                        <h5>Selecione a forma de Pagamento:</h5>
-                            <div className="selectPagF  custom-radio custom-control-inline">
-                                
-                                <select onChange={(e) => pegarValorDaConsulta(idServico, e.target.value)} className="form-control">
-                                    <option value="Dinheiro">Dinheiro</option>
-                                    <option value="Cartão">Cartão</option>
-                                </select>
-                                
-                            </div>
-                        </div>
                     </div>
 
-                    <div className="linePag2F">
-                        <div className="formParcF">
-                            <h5>Quantidade de parcelas:</h5>
-                            <input value={parcelas} type="number" className="qtdParcF form-control" min="1" max="8" 
-                            onChange={e => valorParceladoClick(e.target.value)}
-                            />
+                    <div className="BoxPagmtF">
+                        <h2>Pagamento da consulta</h2>
+
+                        <div className="linePag1F">
+                                <div className="formPagmF">
+                                    <h5>Selecione a forma de Pagamento:</h5>
+                                        <div className="selectPagF  custom-radio custom-control-inline">
+                                            
+                                            <select onChange={(e) => pegarValorDaConsulta(idServico, e.target.value)} className="form-control">
+                                                <option value="Dinheiro">Dinheiro</option>
+                                                <option value="Cartão">Cartão</option>
+                                            </select>
+                                            
+                                        </div>
+                                  </div>
+
+                                <div className="formParcF">
+                                    <h5>Quantidade de parcelas:</h5>
+                                    <input value={parcelas} type="number" className="qtdParcF form-control" min="1" max="8" 
+                                    onChange={e => valorParceladoClick(e.target.value)}
+                                    />
+                                </div>
+
                         </div>
 
-                        <div className="formSubF">
-                            <h5>SubTotal:</h5>
-                            <input type="text" readOnly className="SubtF form-control" placeholder="R$ 0,00" 
-                            value={"R$" + subtotal}
-                            />
-                            
-                        </div>
 
-                        <p className="totalPorMesAgendarF">( {parcelas}X de R${valorParcelado} )</p>
+                        <div className="linePag2F">
+
+                                <div className="formDescF">
+                                   <h5>Desconto:</h5>
+                                   <input type="text" readOnly className="DescF form-control" placeholder="R$ 0,00" 
+                                   value={"R$" + desconto}
+                                   />
+
+                                  <p className="totalPorMesAgendarF">( {parcelas}X de R${valorParcelado} )</p>
+                                </div>
+
+                                <div className="formSubF">
+                                    <h5>SubTotal:</h5>
+                                    <input type="text" readOnly className="SubtF form-control" placeholder="R$ 0,00" 
+                                    value={"R$" + subtotal}
+                                    />
+                                </div>
+                          
+
+                                <div className="formTotF">
+                                    <h5>Total:</h5>
+                                    <input type="text" readOnly className="TotalF form-control" placeholder="R$ 0,00"
+                                    value={"R$" + total}
+                                    />
+                                </div>
+                              
+                          </div>
+
+                          <div className="linePag3F">
+                                <div className="ButtPagF">
+                                    <button onClick={agendarClick} className="btnCadF btn btn-primary">
+                                        <h4>Agendar</h4><img src={Calendar}/></button>
+                                </div>
+                          </div>
+                        
                     </div>
-
-                    <div className="linePagm3F">
-                        <div className="formDescF">
-                            <h5>Desconto:</h5>
-                            <input type="text" readOnly className="DescF form-control" placeholder="R$ 0,00" 
-                            value={"R$" + desconto}
-                            />
-                        </div>
-
-                        <div className="formTotF">
-                            <h5>Total:</h5>
-                            <input type="text" readOnly className="TotalF form-control" placeholder="R$ 0,00"
-                            value={"R$" + total}
-                            />
-                        </div>
-
-                        <div className="ButtPagF">
-                            <button onClick={agendarClick} className="btnCadF btn btn-primary">
-                                <h4>Agendar</h4><img src={Calendar}/></button>
-                        </div>
-                    </div>
-
 
                 </div>
 
