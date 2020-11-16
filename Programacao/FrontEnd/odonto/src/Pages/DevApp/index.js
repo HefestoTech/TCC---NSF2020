9a8a4815a2d74a72cec92aed0a0c05d53aaf8a46
ArthurzinLindo <juvenalfp14@gmail.com>
2020-11-15 23:11:41 -0300

    tela app is End

--- Programacao/FrontEnd/odonto/src/Pages/DevApp/index.js
+++ Programacao/FrontEnd/odonto/src/Pages/DevApp/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import IphoneApp from '../../Assets/Fotos/fotoIphone.png'
+import IphoneApp from '../../Assets/Fotos/Iphone11.png'
 import { Link } from 'react-router-dom'
 
 export default function DevApp(){
@@ -12,9 +12,27 @@ export default function DevApp(){
              <Link to='/'><i class="fas fa-tooth"></i></Link>
             </div>
 
-            <div>
-            <img className="iphone" src={IphoneApp}/>
+            <div className="textApp">
+
+                <h2 className="textH2app">
+                    Sobre o App :
+                </h2>
+
+                <p className="pApp">
+                Bem vindo ao nosso App odonto!
+O odonto App trás para você qualidade, facilidade e confronto ao fazer suas consultas!
+O App permite que você faça agendamentos e conferir os horários de suas consultas de maneira prática e fácil sem que você saia do seu conforto!
+                </p>
+
             </div>
+
+            <div className="Iphonneee">
+            <img className="iphone" src={IphoneApp}/>
+            
+            </div>
+            <p className="aapp">
+                Para mais informações entre em contato pelo e-mail : hefestotech@gmail.com
+            </p>
             <div class="rodapésobre">
                 <p className="rodapezinnn">
                 © Hefesto Tech, 2020 All rights reserved
