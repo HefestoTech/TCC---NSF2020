9dd59d4714a6511199d31600bc9f5b25747ed936
Felipe Conceição <felipecsilva207@gmail.com>
2020-11-21 21:50:13 -0300

    arrumei uns errinhos ai

--- Programacao/FrontEnd/odonto/src/Pages/SobreDev/index.js
+++ Programacao/FrontEnd/odonto/src/Pages/SobreDev/index.js
@@ -22,7 +22,7 @@ export default function SobreDev() {
                 </div>
                 
                 <div class="rOne">
-                   <a href="vhttps://www.instagram.com/hefesto.tec/"><img src = {Instagram}/></a>
+                   <a href="https://www.instagram.com/hefesto.tec/"><img src = {Instagram}/></a>
                 </div>
 
                 <div class="GbOne">
